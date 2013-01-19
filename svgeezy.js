132347c759ac629f26984d953d5011bf342b27d7
Luke Edwards <lexigraphic@gmail.com>
2013-01-19 23:25:51 +1300

    Fix SVG as an img detection for Firefox 3
    Detection was not functioning in Firefox 3 as it does support basic SVG
    functions such as creating a rectangle which was the current detection
    method - the new detection method was taken from this very
    comprehensive article
    http://www.voormedia.nl/blog/2012/10/displaying-and-detecting-support-fo
    r-svg-images.

--- svgeezy.js
+++ svgeezy.js
@@ -52,7 +52,7 @@ var svgeezy = function(){
 		},
 
 		supportsSvg: function(){
-			return document.createElementNS && document.createElementNS( "http://www.w3.org/2000/svg", "svg").createSVGRect;
+			return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")
 		}
 	};
 
