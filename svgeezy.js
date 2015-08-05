61355f50b69d63b9f1a630e66fdafb4973b79885
Nicholas Ruunu <nicholas@ruu.nu>
2015-08-05 19:00:08 +0200

    Makes indentation consistent

--- svgeezy.js
+++ svgeezy.js
@@ -15,54 +15,54 @@
 
 window.svgeezy = function() {
 
-		return {
+	return {
 
-			init: function(avoid, filetype) {
-				this.avoid = avoid || false;
-				this.filetype = filetype || 'png';
-				this.svgSupport = this.supportsSvg();
-				if(!this.svgSupport) {
-					this.images = document.getElementsByTagName('img');
-					this.imgL = this.images.length;
-					this.fallbacks();
-				}
-			},
-
-			fallbacks: function() {
-				while(--this.imgL) {
-					if(this.avoid && this.hasClass(this.images[this.imgL], this.avoid)) {
-						continue;
-					}
-					var src = this.images[this.imgL].getAttribute('src');
-					if(src === null) {
-						continue;
-					}
-					if(this.getFileExt(src) != 'svg') {
-						continue;
-					}
-					var newSrc = src.replace('.svg', '.' + this.filetype);
-					this.images[this.imgL].setAttribute('src', newSrc);
-					
-				}
-			},
-
-			getFileExt: function(src) {
-				var ext = src.split('.').pop();
-
-        			if(ext.indexOf("?") !== -1) {
-          				ext = ext.split('?')[0];
-        			}
-
-        			return ext;
-			},
-
-			hasClass: function(element, cls) {
-				return(' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-			},
-
-			supportsSvg: function() {
-				return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+		init: function(avoid, filetype) {
+			this.avoid = avoid || false;
+			this.filetype = filetype || 'png';
+			this.svgSupport = this.supportsSvg();
+			if(!this.svgSupport) {
+				this.images = document.getElementsByTagName('img');
+				this.imgL = this.images.length;
+				this.fallbacks();
 			}
-		};
+		},
 
-	}();
+		fallbacks: function() {
+			while(--this.imgL) {
+				if(this.avoid && this.hasClass(this.images[this.imgL], this.avoid)) {
+					continue;
+				}
+				var src = this.images[this.imgL].getAttribute('src');
+				if(src === null) {
+					continue;
+				}
+				if(this.getFileExt(src) != 'svg') {
+					continue;
+				}
+				var newSrc = src.replace('.svg', '.' + this.filetype);
+				this.images[this.imgL].setAttribute('src', newSrc);
+				
+			}
+		},
+
+		getFileExt: function(src) {
+			var ext = src.split('.').pop();
+
+			if(ext.indexOf("?") !== -1) {
+				ext = ext.split('?')[0];
+			}
+
+			return ext;
+		},
+
+		hasClass: function(element, cls) {
+			return(' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+		},
+
+		supportsSvg: function() {
+			return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+		}
+	};
+
+}();
